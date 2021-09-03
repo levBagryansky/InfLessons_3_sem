d27e8ad1e54a506c641860fe9f46c5ba7185750c
Tardis3 <bagrianskii.liu@phystech.edu>
2021-09-03 10:08:24 +0300

    .

---
+++ lesson03.09.21/cmake-build-debug/CMakeFiles/lesson03_09_21.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lesson03_09_21.dir/main.cpp.obj"
+  "lesson03_09_21.exe"
+  "lesson03_09_21.exe.manifest"
+  "lesson03_09_21.pdb"
+  "liblesson03_09_21.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lesson03_09_21.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lesson03.09.21/cmake-build-debug/CMakeFiles/lesson03_09_21.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for lesson03_09_21.
+# This may be replaced when dependencies are built.
