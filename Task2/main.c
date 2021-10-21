21b9f4252f3b4d77008bd71d3ae3aa71a87597fd
levBagryansky <28lev11@gmail.com>
2021-10-21 16:28:12 +0300

    Парсинг файла на матрицу по словам и |

--- Task2/main.c
+++ Task2/main.c
@@ -9,14 +9,40 @@ int get_file_size(int fd);
 char* file2arr(int fd, int* arrLen);
 int numWords(char* arr,int lenArr);
 int getWordLen(char* arr, int position, int arrLen);
+int nextWord(char* arr, int position, int arrLen){
+    while (position < arrLen){
+        if (!isspace(arr[position])){
+            break;
+        }
+        position++;
+    }
+
+    return position;
+}
+
 char* getWord(char* arr, int position, int arrLen){
     ;
 }
 char** arr2matrix(char* arr,int lenArr, int* pSize){
     int nWords = numWords(arr, lenArr);
+    *pSize = nWords;
     char **matrix = (char**) calloc(nWords, sizeof(char*));
-    return (char **)arr;
+    int position = 0;
+    for(int i = 0; i < nWords; i++){
+        int lenWord = getWordLen(arr, position, lenArr);
+        matrix[i] = (char*) calloc(lenWord + 1, sizeof(char));
+        position = nextWord(arr, position, lenArr);
+        for (int j = 0; j < lenWord; ++j) {
+            matrix[i][j] = arr[position];
+            position ++;
+        }
+    }
+
+    return matrix;
 }
+
+void printMatrix(char** matrix, int size);
+
 int numOfBar(char *arr, int *plen); //возвращает ближайший |
 int lastBar(char *arr, int len);
 void doBar(char *arr, int len){
@@ -31,7 +57,10 @@ int main(int argc, char** argv) {
     }
     int len;
     char *arr = file2arr(fd_from, &len);
-
+    //printf("Next word on the %i position\n", nextWord(arr, 31, len));
+    int sizeMatrix;
+    char** matrix = arr2matrix(arr, len, &sizeMatrix);
+    printMatrix(matrix, sizeMatrix);
     return 0;
 }
 
@@ -93,7 +122,7 @@ int numWords(char* arr,int lenArr){
 
 int getWordLen(char* arr, int position, int arrLen){
     while (position < arrLen && isspace(arr[position])){
-        printf( "Hello there\n");
+        //printf( "Hello there\n");
         position++;
     }
 
@@ -112,6 +141,12 @@ int getWordLen(char* arr, int position, int arrLen){
     }
 
     return result;
+}
+
+void printMatrix(char** matrix, int size){
+    for (int i = 0; i < size; ++i) {
+        printf("%s\n", matrix[i]);
+    }
 }
 
 int numOfBar(char *arr, int *plen){
