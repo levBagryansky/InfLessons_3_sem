d9d1acbd0c95482fd0e7bd78fe821e293f7cf7a0
levBagryansky <28lev11@gmail.com>
2021-11-25 17:36:57 +0300

    Not working

--- SharedStack/stack_t.c
+++ SharedStack/stack_t.c
@@ -10,74 +10,61 @@
 #include <time.h>
 
 struct timespec ts = {0, 1e6};
+struct sembuf sem_unlock = {0, 1, 0};
+struct sembuf sem_lock = {0, -1, 0};
 
 stack_t* attach_stack(key_t key, int size){
-	/*
-	struct sembuf sem_lock = {0, -1, 0};
-	int sem_id = semget(key, 1, IPC_CREAT | IPC_EXCL | 0666);
-	if(sem_id == -1){ // семафор уже существует
-		sem_id = semget(key, 1, IPC_CREAT | 0666);
-		//semop(sem_id, &sem_lock, 1);
-	}else {+
-		semctl(sem_id, 	0, SETVAL, 1);
-	}
-	//semtimedop(sem_id, &sem_lock, 1, &ts);
-	int semoped = semop(sem_id, &sem_lock, 1);
-	printf("AAAAAAAsemop = %i\n", semoped);
-	printf("Lock and attach stack\n");*/
 	stack_t *result;
-	int shm_stack_t_id = shmget(key, sizeof(void *) * size + sizeof(stack_t), IPC_CREAT | IPC_EXCL | 0666);
-	if(shm_stack_t_id != -1) {// stack does not exist
-		result = (stack_t *) shmat(shm_stack_t_id, NULL, 0);
+	int sem_id = semget(key, 1, 0666| IPC_CREAT | IPC_EXCL);
+	if(sem_id == -1){// семафор сущесвтует
+		//semctl(sem_id, 0, SETVAL, 1);
+		sem_id = semget(key, 1, 0666| IPC_CREAT);
+		semop(sem_id, &sem_lock, 1);
+		semctl(sem_id, 0, SETVAL, 0);
+	}
+	else { // семафор не существует
+		semctl(sem_id, 0, SETVAL, 1);
+		semop(sem_id, &sem_lock, 1); // теперь можно быть увереным что никто параллельно ничего не делает
+		semctl(sem_id, 0, SETVAL, 0);
+	}
+
+	int shm_id = shmget(key, sizeof(void *) * size + sizeof(stack_t), 0666| IPC_CREAT | IPC_EXCL);
+	if (shm_id == -1){ // stack exists => it mustn't be deleted
+		printf("Stack exists, just shmat...\n");
+		shm_id = shmget(key, sizeof(void *) * size + sizeof(stack_t), 0666| IPC_CREAT);
+		result = (stack_t*)shmat(shm_id, NULL, 0);
+		result->n_process ++;
+		printf("shmated\n");
+	} else{
+		printf("Stack does not exists, creating...\n");
+		result = (stack_t* )shmat(shm_id, NULL, 0);
 		memset(result, 0, sizeof(void *) * size + sizeof(stack_t));
 		result->n_process++;
-		if(result == (stack_t *)(-1)){
-			printf("shmat returned NULL\n");
-		}
-		printf("in attach %i\n", shm_stack_t_id);
-
-		result->sem_id = semget(key, 1, IPC_CREAT | 0666);
-		semctl(result->sem_id, 0, SETVAL, 1);
-		//Lock(result);
-		result->size = size;
+		result->sem_id = sem_id;
 		result->count = 0;
-		result->shm_stack_t_id = shm_stack_t_id;
+		result->size = size;
 		result->data = (void **) &(result->data) + 1;
-	} else{ //stack exists
-		shm_stack_t_id = shmget(key, sizeof(void *) * size + sizeof(stack_t), IPC_CREAT | 0666);
-		result = (stack_t *) shmat(shm_stack_t_id, NULL, 0);
-		result->n_process++;
-		if(result == (stack_t *)(-1)){
-			printf("shmat returned NULL\n");
-		}
-		result->sem_id = semget(key, 1, IPC_CREAT | 0666);
-		printf("sem_id = %i\n", result->sem_id);
-		//Lock(result);
+		result->shm_stack_t_id = shm_id;
+		printf("created\n");
 	}
 
-	usleep(100000);
-	//result -> n_process ++;
-	//Unlock(result);
-	//pthread_mutex_unlock(&result->mutex);
+	semop(sem_id, &sem_unlock, 1);
 	return result;
 }
 
 void Lock(stack_t* stack){
 	printf("In Lock, sem_id = %i\n", stack->sem_id);
-	struct sembuf sem_lock = {0, -1, 0};
 	semop(stack -> sem_id, &sem_lock, 1);
 }
 
 void Unlock(stack_t* stack){
 	printf("In Unlock, sem_id = %i\n", stack->sem_id);
-	struct sembuf sem_unlock = {0, 1, 0};
 	semop(stack -> sem_id, &sem_unlock, 1);
 }
 
 int push(stack_t* stack, void* val){
 	//semctl(stack->sem_id, 0, GETVAL);
 	printf("In Lock Push, sem_id = %i\n", stack->sem_id);
-	struct sembuf sem_lock = {0, -1, 0};
 	semop(stack -> sem_id, &sem_lock, 1);
 	if(stack->count == stack->size){
 		return -1;
@@ -124,30 +111,36 @@ int mark_destruct(stack_t* stack){
 	Lock(stack);
 	printf("Marking destruct\n");
 	stack-> to_destruct ++;
-	int res = shmctl(stack->shm_stack_t_id, IPC_RMID, NULL);
+	//int res = shmctl(stack->shm_stack_t_id, IPC_RMID, NULL);
 	Unlock(stack);
-	return res;
+	return 0;
 }
 
 int detach_stack(stack_t* stack){
 	printf("In Lock Detach, sem_id = %i\n", stack->sem_id);
-	struct sembuf sem_lock = {0, -1, 0};
-	semop(stack -> sem_id, &sem_lock, 1);	stack->n_process --;
+	stack -> n_process --;
+	semop(stack -> sem_id, &sem_lock, 1);
 	printf("stack->n_process = %d, stack -> to_destruct = %d\n", stack->n_process, stack -> to_destruct);
 	if(stack->n_process == 0 && stack -> to_destruct) {
-		//pthread_mutex_destroy(&stack->mutex);
-		int buf_sem_id = stack->sem_id;
-		shmdt(stack);
-		//Unlock(stack);
-		struct sembuf sem_unlock = {0, 1, 0};
-		printf("Unlock and deleting stack\n");
-		semctl(buf_sem_id, 0, IPC_RMID, NULL);
-		semop(buf_sem_id, &sem_unlock, 1);
+		//usleep(1e5);
+		if(stack->n_process == 0 && stack -> to_destruct) {
+			//pthread_mutex_destroy(&stack->mutex);
+			int buf_sem_id = stack->sem_id;
+			shmctl(stack->shm_stack_t_id, IPC_RMID, NULL);
+			shmdt(stack);
+			//Unlock(stack);
+			semctl(buf_sem_id, 0, IPC_RMID, NULL);
+			printf("Unlock and deleting stack\n");
+			semop(buf_sem_id, &sem_unlock, 1);
+		}else{
+			int buf_sem_id = stack->sem_id;
+			shmdt(stack);
+			printf("Unlock and detaching stack\n");
+			semop(buf_sem_id, &sem_unlock, 1);
+		}
 	} else{
 		int buf_sem_id = stack->sem_id;
 		shmdt(stack);
-		//Unlock(stack);
-		struct sembuf sem_unlock = {0, 1, 0};
 		printf("Unlock and detaching stack\n");
 		semop(buf_sem_id, &sem_unlock, 1);
 	}
